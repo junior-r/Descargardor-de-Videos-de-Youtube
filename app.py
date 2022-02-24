334bd9f3015282acc0939c8771836b9293e305c6
Junior Ruiz <junior31064049@gmail.com>
2022-02-24 09:52:21 -0400

    Se añadió formatos, descarga de video y la ruta

--- app.py
+++ app.py
@@ -2,5 +2,18 @@ from pytube import YouTube
 
 url = input('Ingrese la URL del video a Descargar: ')
 video = YouTube(url)
-# Esto listara todos los formatos de descarga disponible para el vídeo
-videos = video.streams.all()
+# Esto transmitirá todos los formatos de descarga disponible para el vídeo
+try:
+  videos = video.streams.all()
+  # Esto enumera los formatos disponibles del video
+  formatos = list(enumerate(videos))
+  for i in formatos:
+    print(i)
+    # Esto imprimir'a los formatos de descarga disponibles
+
+  dn_opcion = int(input('Ingrese el número correspondiente a la calidad del video para descargar: '))
+  dn_video = videos[dn_opcion]
+  dn_video.download('C:/Users/USER/Downloads')
+  print('Descarga exitosa!')
+except:
+  print('Ha ocurrido un error')
