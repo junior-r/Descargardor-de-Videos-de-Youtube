d5b46342d4bc02acae1a5576c90fd8161caefa53
Junior Ruiz <junior31064049@gmail.com>
2022-02-24 11:21:51 -0400

    Actualizando

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 from pytube import YouTube
 
-url = input('Ingrese la URL del video a Descargar: ')
-video = YouTube(url)
-# Esto transmitirá todos los formatos de descarga disponible para el vídeo
-try:
-  videos = video.streams.all()
-  # Esto enumera los formatos disponibles del video
-  formatos = list(enumerate(videos))
-  for i in formatos:
-    print(i)
-    # Esto imprimir'a los formatos de descarga disponibles
 
-  dn_opcion = int(input('Ingrese el número correspondiente a la calidad del video para descargar: '))
-  dn_video = videos[dn_opcion]
-  dn_video.download('C:/Users/USER/Downloads')
-  print('Descarga exitosa!')
+url_video = input('Ingrese la URL del video a Descargar: ')
+destino = input('Ingrese el destino donde desea descargar el archivo: ')
+
+yt = YouTube(url_video)
+name_video = yt.title
+print('Se descargará el Archivo:', name_video)
+videos = yt.streams.all()
+
+video = list(enumerate(videos))
+for i in video:
+  print(i)
+
+dn_opcion = int(input('Ingrese el Número correspondiente al formato para Descargar: '))
+dn_video = videos[dn_opcion]
+try:
+  dn_video.download(destino)
+  print(f'Se ha descargado su video en {destino}')
 except:
   print('Ha ocurrido un error')
